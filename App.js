5f3f5f43031e76ffc1ab6c757bc1c1095d83268b
Stelios <stelios.phan@gmail.com>
2020-07-12 04:21:23 -0400

    Turned button in list into separate component

--- App.js
+++ App.js
@@ -1,26 +1,14 @@
 import React, { useState } from "react";
-import { StyleSheet, View, Text, TouchableHighlight } from "react-native";
-
-function RewardsButton({company, progress, outOf, onPress = f => f}){
-  return (
-    <TouchableHighlight 
-      style={styles.button}
-      onPress={()=>onPress("Hulu")}
-      underlayColor="skyblue"
-    >
-      <View>
-        <Text style={styles.buttonTitle}>{company}</Text>
-        <Text style={styles.buttonText}>{progress}/{outOf} Cups</Text>
-      </View>
-    </TouchableHighlight>
-  )
-}
+import { StyleSheet, View } from "react-native";
+import RewardsCard from "./components/RewardsCard"
 
 export default function App() {
   const [company, setCompany] = useState();
   return (
     <View style={styles.container}>
-      <RewardsButton company="Starbucks" progress="0" outOf="10" onPress={setCompany} />
+      <RewardsCard company="Starbucks" progress="0" outOf="10" onPress={()=>{}} />
+      <RewardsCard company="Kung Fu Tea" progress="4" outOf="10" onPress={()=>{}} />
+      <RewardsCard company="Momo's" progress="8" outOf="10" onPress={()=>{}} />
     </View>
   );
 }
@@ -31,23 +19,5 @@ const styles = StyleSheet.create({
     display: "flex",
     justifyContent: "center",
     alignItems: "center"
-  },
-  button: {
-    height: 150,
-    margin: 10,
-    padding: 10,
-    borderWidth: 2,
-    borderRadius: 10,
-    justifyContent: "top",
-    alignSelf: "stretch",
-    backgroundColor: "rgba(255,255,255,0.8)"
-  },
-  buttonTitle:{
-    fontSize: 30,
-    textAlign: "left"
-  },
-  buttonText:{
-    fontSize: 16,
-    textAlign: "left"
   }
 });
