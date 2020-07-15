699898f9051c8531a69b89e3c6ece9aec0a2bbd8
SteliosPhanartzis <stelios.phan@gmail.com>
2020-07-14 23:35:41 -0400

    Setting up front end

--- App.js
+++ App.js
@@ -1,27 +1,13 @@
 import React, { useState } from "react";
-import { StyleSheet, View, Text, TouchableHighlight } from "react-native";
-
-function RewardsButton({company, progress, outOf, onPress = f => f}){
-  return (
-    <TouchableHighlight 
-      style={styles.button}
-      onPress={()=>onPress("Hulu")}
-      underlayColor="skyblue"
-    >
-      <View>
-        <Text style={styles.buttonTitle}>{company}</Text>
-        <Text style={styles.buttonText}>{progress}/{outOf} Cups</Text>
-      </View>
-    </TouchableHighlight>
-  )
-}
+import { StyleSheet, ScrollView } from "react-native";
+import RewardsButton from "./components/RewardsButton";
 
 export default function App() {
   const [company, setCompany] = useState();
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
       <RewardsButton company="Starbucks" progress="0" outOf="10" onPress={setCompany} />
-    </View>
+    </ScrollView>
   );
 }
 
@@ -31,23 +17,5 @@ const styles = StyleSheet.create({
     display: "flex",
     justifyContent: "center",
     alignItems: "center"
-  },
-  button: {
-    height: 150,
-    margin: 10,
-    padding: 10,
-    borderWidth: 2,
-    borderRadius: 10,
-    justifyContent: "top",
-    alignSelf: "stretch",
-    backgroundColor: "rgba(255,255,255,0.8)"
-  },
-  buttonTitle:{
-    fontSize: 30,
-    textAlign: "left"
-  },
-  buttonText:{
-    fontSize: 16,
-    textAlign: "left"
   }
 });
---
+++ components/RewardsButton.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { StyleSheet, View, Text, TouchableHighlight, Image } from "react-native";
+
+export default function RewardsButton({company, progress, outOf, onPress = f => f}){
+    return (
+      <TouchableHighlight 
+        style={styles.button}
+        onPress={()=>onPress("Hulu")}
+        underlayColor="skyblue"
+      >
+        <View style={styles.columnAlign}>
+          <Image style={styles.tinyLogo} source={require('@expo/snack-static/react-native-logo.png')}/>
+          <View>
+            <Text style={styles.buttonTitle}>{company}</Text>
+            <Text style={styles.buttonText}>{progress}/{outOf} Cups</Text>
+          </View>
+        </View>
+      </TouchableHighlight>
+    )
+  }
+  
+  const styles = StyleSheet.create({
+    button: {
+      height: 100,
+      margin: 10,
+      padding: 10,
+      borderWidth: 2,
+      borderRadius: 10,
+      alignSelf: "stretch",
+      backgroundColor: "rgba(255,255,255,0.8)"
+    },
+    tinyLogo: {
+      width: 45,
+      height: 45,
+      margin: 15
+    },
+    columnAlign: {
+      flex: 1,
+      flexDirection: "row"
+    },
+    buttonTitle:{
+      fontSize: 30,
+      textAlign: "left",
+      marginTop: 5
+    },
+    buttonText:{
+      fontSize: 16,
+      textAlign: "left"
+    }
+  });
